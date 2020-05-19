5dc7fb52292c0e7faf069ea81b475d4168423ee2
yin <605877291@qq.com>
2020-05-19 23:30:12 +0800

    初始化项目

---
+++ jianzhiapp/jianzhiapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jianzhiapp
+//
+//  Created by 殷义军 on 2020/5/19.
+//  Copyright © 2020 殷义军. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
